a93c5117d02584e6825620b539066eafabd9b60e
Jonathan Milot <jonathan@transit.app>
2022-08-16 11:14:54 -0400

    add basic test

---
+++ tests/test_runner.py
@@ -0,0 +1,28 @@
+import pytest
+
+from pathlib import Path
+import gtfs_loader
+from gtfs_loader import test_support
+from gtfs_flex_to_gofs_lite import gofs_lite_converter
+
+import time
+time.time = lambda: 0
+
+test_support.init(__file__)
+
+
+@pytest.mark.parametrize('feed_dir',
+                         test_support.find_tests(),
+                         ids=lambda test_dir: test_dir.name)
+def test_default(feed_dir):
+    do_test(feed_dir)
+
+
+def do_test(feed_dir):
+    work_dir = test_support.create_test_data(feed_dir)
+    dest_dir = work_dir / "output"
+    Path.mkdir(dest_dir)
+
+    gtfs = gtfs_loader.load(work_dir)
+    gofs_lite_converter.convert_to_gofs_lite(gtfs, dest_dir, 24 * 60 * 60, '')
+    test_support.check_expected_output(feed_dir, dest_dir)
