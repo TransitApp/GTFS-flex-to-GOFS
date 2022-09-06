c90a04acdf89e6ecd1e26d4dde513431634310de
Jonathan Milot <jonathan@transit.app>
2022-09-06 16:22:37 -0400

    clean up

--- gtfs_flex_to_gofs_lite/files/wait_times.py
+++ gtfs_flex_to_gofs_lite/files/wait_times.py
@@ -1,6 +1,3 @@
-from dataclasses import dataclass
-from typing import List
-
 from ..gofs_file import GofsFile
 
 FILENAME = 'wait_times'
@@ -8,5 +5,4 @@ FILENAME = 'wait_times'
 
 def create(gtfs, pickup_booking_rule_ids):
     # Implement to support this file
-
     return GofsFile(FILENAME, created=False)
