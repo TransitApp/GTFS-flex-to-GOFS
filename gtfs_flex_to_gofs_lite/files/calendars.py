1a58e3e22c5aef057dbf880c9693dfd861024bd9
Guillaume Campagna <guillaume.campagna@gmail.com>
2024-11-19 14:45:07 -0500

    Support added calendar_dates as long as they are not mixed with regular calendar

--- gtfs_flex_to_gofs_lite/files/calendars.py
+++ gtfs_flex_to_gofs_lite/files/calendars.py
@@ -3,9 +3,19 @@ from gtfs_loader import schema
 from typing import List
 
 from ..gofs_file import GofsFile
+from datetime import datetime, timedelta
 
 FILENAME = 'calendars'
 
+DAYS_OF_WEEK = {
+    'monday': 'mon',
+    'tuesday': 'tue',
+    'wednesday': 'wed',
+    'thursday': 'thu',
+    'friday': 'fri',
+    'saturday': 'sat',
+    'sunday': 'sun'
+}
 
 @dataclass
 class Calendar:
@@ -22,21 +32,7 @@ def create(gtfs, used_calendar_ids):
         if calendar.service_id not in used_calendar_ids:
             continue  # Only extract calender that are actually used by on demand services
 
-        days = []
-        if calendar.monday:
-            days.append('mon')
-        if calendar.tuesday:
-            days.append('tue')
-        if calendar.wednesday:
-            days.append('wed')
-        if calendar.thursday:
-            days.append('thu')
-        if calendar.friday:
-            days.append('fri')
-        if calendar.saturday:
-            days.append('sat')
-        if calendar.sunday:
-            days.append('sun')
+        days = extract_service_days(calendar)
 
         excepted_dates = []
         if calendar.service_id in gtfs.calendar_dates:
@@ -45,12 +41,57 @@ def create(gtfs, used_calendar_ids):
                     excepted_dates.append(repr(calendar_date.date))
 
                 if calendar_date.exception_type is schema.ExceptionType.ADD:
-                    # TODO
-                    pass
+                    raise ValueError(f"Added dates are not supported, we need to support it in GOFS-lite first: {calendar.service_id}")
 
         calendar_data = Calendar(calendar.service_id, repr(
             calendar.start_date), repr(calendar.end_date), days, excepted_dates)
+        used_calendar_ids.remove(calendar.service_id)
 
         calendars_data.append(calendar_data)
 
+    for calendar_id in used_calendar_ids:
+        # Remaining calendar ids should be from calendar_dates, otherwise they are just missing
+        if calendar.service_id in gtfs.calendar_dates:
+            active_dates_calendar = []
+            for calendar_date in gtfs.calendar_dates[calendar_id]:
+                if calendar_date.exception_type is schema.ExceptionType.ADD:
+                    if repr(calendar_date.date) not in active_dates_calendar:
+                        active_dates_calendar.append(repr(calendar_date.date))
+                elif calendar_date.exception_type is schema.ExceptionType.REMOVE:
+                    if repr(calendar_date.date) in active_dates_calendar:
+                        active_dates_calendar.remove(repr(calendar_date.date))
+
+            print(f"Calendar {calendar_id} has no calendar, but has calendar_dates. Active dates: {active_dates_calendar}")
+
+            # there's no support for a list of supported dates in GOFS-lite
+            # instead, create a calendar from the start to the end date, then remove all the dates that are not in the active dates
+            active_dates_calendar.sort()
+            start_date = active_dates_calendar[0]
+            end_date = active_dates_calendar[-1]
+
+            start_date_dt = datetime.strptime(start_date, '%Y%m%d')
+            end_date_dt = datetime.strptime(end_date, '%Y%m%d')
+
+            all_dates = set()
+            current_date = start_date_dt
+            while current_date <= end_date_dt:
+                all_dates.add(current_date.strftime('%Y%m%d'))
+                current_date += timedelta(days=1)
+
+            active_dates_set = set(active_dates_calendar)
+            missing_dates = all_dates - active_dates_set
+
+            excepted_dates = sorted(list(missing_dates))
+
+            days = list(DAYS_OF_WEEK.values())
+            calendar_data = Calendar(calendar_id, start_date, end_date, days, excepted_dates)
+            calendars_data.append(calendar_data)
+
     return GofsFile(FILENAME, created=True, data=calendars_data)
+
+def extract_service_days(calendar):
+    days = []
+    for day_attr, day_str in DAYS_OF_WEEK.items():
+        if getattr(calendar, day_attr):
+            days.append(day_str)
+    return days
