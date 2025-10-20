fae16959abcbdcb0ca85f4ed0882d89e3c3fecb5
Jeremy Steele <jeremy.ma.steele@gmail.com>
2025-10-20 16:56:46 -0400

    Cleanup patch_gtfs

--- gtfs_flex_to_gofs_lite/patch_gtfs.py
+++ gtfs_flex_to_gofs_lite/patch_gtfs.py
@@ -6,9 +6,9 @@ from .files import operation_rules
 def patch_gtfs(args, gtfs, itineraries=False):
     clean_up_gtfs(gtfs, itineraries)
 
-    if itineraries == False and len(gtfs.stop_times) != 0:
-        gtfs_loader.patch(gtfs, args.gtfs_dir, args.out_gtfs_dir)
-    elif itineraries == True and len(gtfs.itinerary_cells) != 0:
+    is_empty = len(gtfs.itinerary_cells) == 0 if itineraries else len(gtfs.stop_times) == 0 
+
+    if not is_empty:
         gtfs_loader.patch(gtfs, args.gtfs_dir, args.out_gtfs_dir)
     else:
         # we have no data, nothing to export
@@ -16,22 +16,20 @@ def patch_gtfs(args, gtfs, itineraries=False):
 
 
 def clean_up_gtfs(gtfs, itineraries=False):
-    if itineraries:
-        for trip in gtfs.trips.values():
-            type_of_trip = operation_rules.get_type_of_itinerary_trip(trip)
-            if type_of_trip == operation_rules.TripType.OTHER:
+    for trip in gtfs.trips.values():
+        stop_times = gtfs.itinerary_cells[trip.itinerary_index] if itineraries else gtfs.stop_times[trip.trip_id]
+        if itineraries:
+            for i, st in enumerate(stop_times):
+                st.start_pickup_drop_off_window = trip.start_pickup_drop_off_windows[i]
+                st.end_pickup_drop_off_window = trip.end_pickup_drop_off_windows[i]
+        
+        type_of_trip = operation_rules.get_type_of_itinerary_trip(trip)
+        if type_of_trip == operation_rules.TripType.OTHER:
                 print(f"WARNING : Trip {trip.trip_id} is not a normal gtfs trip, not microtransit and not deviated service only. We are not supproting this yet, it will be removed from the GTFS and not shown anywhere")
             
-            if type_of_trip == operation_rules.TripType.OTHER or type_of_trip == operation_rules.TripType.PURE_MICROTRANSIT:
-                del gtfs.trips[trip.trip_id]
+        if type_of_trip == operation_rules.TripType.OTHER or type_of_trip == operation_rules.TripType.PURE_MICROTRANSIT:
+            del gtfs.trips[trip.trip_id]
+            if itineraries and trip.itinerary_index in gtfs.itinerary_cells:
                 del gtfs.itinerary_cells[trip.itinerary_index]
-
-    else:
-        for trip_id, stop_times in list(gtfs.stop_times.items()):
-            type_of_trip = operation_rules.get_type_of_trip(stop_times)
-            if type_of_trip == operation_rules.TripType.OTHER:
-                print(f"WARNING : Trip {trip_id} is not a normal gtfs trip, not microtransit and not deviated service only. We are not supproting this yet, it will be removed from the GTFS and not shown anywhere")
-
-            if type_of_trip == operation_rules.TripType.OTHER or type_of_trip == operation_rules.TripType.PURE_MICROTRANSIT:
-                del gtfs.trips[trip.trip_id]
-                del gtfs.stop_times[trip_id]
+            else:
+                del gtfs.stop_times[trip.trip_id]
